5b5688fad3fd74b9dac85e258976cd43524f5f5b
Seb19861986 <sebastien.uyttenhoef@vandenbroele.be>
2025-02-17 16:09:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -62,5 +62,5 @@ def upload_file():
 
 if __name__ == "__main__":
     # Utiliser le port fourni par Render ou 5000 par défaut
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
